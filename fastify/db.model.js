eee3eaf3c31622075f28baa2259bcbe885fe7b17
pradeepbgs <kumarpradeepbgs@gmail.com>
2024-10-27 09:42:12 +0530

    nothing

--- fastify/db.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-  },
-});
-
-export const User = mongoose.model("User", userSchema);
--- fastify/midl.js
+++
@@ -1,9 +0,0 @@
-export const midl1 = (req,res,next) => {
-    console.log("midl 1")
-    next();
-}
-
-export const midl2 = (req,res,next) => {
-    console.log("midl 2")
-    next();
-}
